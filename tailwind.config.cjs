8486af64ddaad35bf121b6b577dfa9949a301ede
JosephRidge <josephridge18@gmail.com>
2022-07-25 22:07:16 +0300

    updated

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,ts,html,js}"],
   theme: {
     extend: {},
   },
