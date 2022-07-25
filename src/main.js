643ca0034ad80898d7afd7f6ccbb60bfcd8bc88e
JosephRidge <josephridge18@gmail.com>
2022-07-25 21:11:12 +0300

    Home Page setup

--- src/main.js
+++ src/main.js
@@ -1,5 +1,21 @@
 import { createApp } from 'vue'
-// import './style.css'
+import {createRouter, createWebHistory} from 'vue-router'
+import './style/index.css'
+import Home from './components/HomePage.vue'
+
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const routes = [
+    {
+      path:'/',
+      name:'Home',
+      component:Home, 
+   
+    },]
+// create the router instance 
+const router = createRouter({
+    history:createWebHistory(), 
+    routes,
+  })
+ 
+createApp(App).use(router).mount('#app')
