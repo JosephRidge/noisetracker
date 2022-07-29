17f1ba046f4a8aa04f4efd69771073e0ace5f67e
JosephRidge <josephridge18@gmail.com>
2022-07-30 00:46:38 +0300

    update

--- src/main.js
+++ src/main.js
@@ -7,6 +7,8 @@ import Analytics from './components/Analytics.vue'
 
 import App from './App.vue' 
 import HighchartsVue from 'highcharts-vue'
+
+
 const routes = [
     {
       path:'/',
@@ -30,6 +32,28 @@ const router = createRouter({
     routes,
   })
  
+// Firebase 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app"; 
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAiglHw8MoQGXGU_fuFY3QEGXtRNobTlwA",
+  authDomain: "ecouteio.firebaseapp.com",
+  databaseURL: "https://ecouteio-default-rtdb.firebaseio.com",
+  projectId: "ecouteio",
+  storageBucket: "ecouteio.appspot.com",
+  messagingSenderId: "999458634943",
+  appId: "1:999458634943:web:fedc09d9d66731ced51a0a",
+  measurementId: "G-LNX9Q036VS"
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig); 
+
 createApp(App)
 .use(HighchartsVue)
 .use(router)
